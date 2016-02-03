f4a9769cc10e5e03e58f6814a69697dbb70637b5
GDRMc <gdrdu86@gmail.com>
2016-02-04 00:41:34 +0100

    TP3 Banque - Code OK, javadoc TODO

--- src/gdr/tp/tp3/Banque.java
+++ src/gdr/tp/tp3/Banque.java
@@ -3,20 +3,20 @@ package gdr.tp.tp3;
 import java.util.ArrayList;
 
 public class Banque {
-    
+
     private String nomBanque;
-    
+
     private ArrayList<Client> clients;
-    
+
     /**
      *
      * @param nomBanque
      */
-    public Banque(String nomBanque){
+    public Banque(String nomBanque) {
         this.nomBanque = nomBanque;
         this.clients = new ArrayList();
     }
-    
+
     /**
      *
      * @param soldeInitial
@@ -24,26 +24,26 @@ public class Banque {
      * @param nomClient
      * @return
      */
-    public CompteBancaireRemunere creerCompteRemunere(double soldeInitial, double tauxInteret, String nomClient){
+    public CompteBancaireRemunere creerCompteRemunere(double soldeInitial, double tauxInteret, String nomClient) {
         Client c = this.rechercheClient(nomClient);
         boolean insert = false;
         CompteBancaireRemunere compte = null;
-        if(c!=null){
-            for(int i=0;i<this.clients.size();i++){
-                if(this.clients.get(i).donneNom().equals(c.donneNom())){
-                    compte = new CompteBancaireRemunere(soldeInitial,tauxInteret, c);
+        if (c != null) {
+            for (int i = 0; i < this.clients.size(); i++) {
+                if (this.clients.get(i).donneNom().equals(c.donneNom())) {
+                    compte = new CompteBancaireRemunere(soldeInitial, tauxInteret, c);
                     this.clients.get(i).ajouteCompte(compte);
                     insert = true;
                 }
             }
         }
-        if(insert){
+        if (insert) {
             return compte;
         } else {
             return null;
         }
     }
-    
+
     /**
      *
      * @param nomEntreprise
@@ -51,16 +51,16 @@ public class Banque {
      * @param adresse
      * @return
      */
-    public Entreprise creerEntreprise(String nomEntreprise, int numeroSIRET, String adresse){
+    public Entreprise creerEntreprise(String nomEntreprise, int numeroSIRET, String adresse) {
         //this.clients.add(new Entreprise(nomEntreprise, adresse, numeroSIRET));
         Entreprise e = new Entreprise(nomEntreprise, adresse, numeroSIRET);
-        if(!this.clientExiste(e)){
+        if (!(this.rechercheClient(nomEntreprise) == e)) {
             //si le client n'existe pas
             this.clients.add(e);
         }
         return e;
     }
-    
+
     /**
      *
      * @param nomParticulier
@@ -68,85 +68,91 @@ public class Banque {
      * @param adresseParticulier
      * @return
      */
-    public Particulier creerParticulier(String nomParticulier, String prenomParticulier, String adresseParticulier){
+    public Particulier creerParticulier(String nomParticulier, String prenomParticulier, String adresseParticulier) {
         Particulier p = new Particulier(nomParticulier, prenomParticulier, adresseParticulier);
-        if(!this.clientExiste(p)){
-            //si le client n'existe pas
-            this.clients.add(p);
+        boolean ok = false;
+        if (this.rechercheClient(nomParticulier) == null) {
+            ok = true;
         }
-        return p;
+        if (ok) {
+            this.clients.add(p);
+            return p;
+        } else {
+            return null;
+        }
     }
-    
+
     /**
      *
      * @param nom
      * @return
      */
-    public Client rechercheClient(String nom){
-        int clientN = -1;
-        for(int i=0;i<this.clients.size();i++){
-            if(this.clients.get(i).donneNom().equals(nom)){
-                clientN = i;
+    public Client rechercheClient(String nom) {
+        boolean trouve = false;
+        int pos = -1;
+        for (int i = 0; i < this.clients.size(); i++) {
+            if (this.clients.get(i).donneNom().equals(nom)) {
+                trouve = true;
+                pos = i;
             }
         }
-        if(clientN == -1){
-            return this.clients.get(clientN);
-        }
-        else {
+        if (trouve) {
+            return this.clients.get(pos);
+        } else {
             return null;
         }
     }
-    
+
     /**
      *
      * @param num
      * @return
      */
-    public CompteBancaire rechercheCompte(int num){
+    public CompteBancaire rechercheCompte(int num) {
         int itrouve = -1;
         int jtrouve = -1;
         CompteBancaire compte;
-        for(int i=0;i<this.clients.size();i++){
-            for(int j=0;j<this.clients.get(i).donneComptes().size();i++){
-                if(this.clients.get(i).donneComptes().get(j).donneNumero() == num){
+        for (int i = 0; i < this.clients.size(); i++) {
+            for (int j = 0; j < this.clients.get(i).donneComptes().size(); i++) {
+                if (this.clients.get(i).donneComptes().get(j).donneNumero() == num) {
                     itrouve = i;
                     jtrouve = j;
                 }
             }
         }
-        if(itrouve!=-1&&jtrouve!=-1){
+        if (itrouve != -1 && jtrouve != -1) {
             compte = this.clients.get(itrouve).donneComptes().get(jtrouve);
         } else {
             compte = null;
         }
         return compte;
     }
-    
+
     /**
      *
      * @param nomClient
      * @return
      */
-    public ArrayList<CompteBancaire> rechercheCompte(String nomClient){
+    public ArrayList<CompteBancaire> rechercheCompte(String nomClient) {
         Client c = this.rechercheClient(nomClient);
         boolean clientExiste = this.clientExiste(c);
-        if(clientExiste){
+        if (clientExiste) {
             return c.donneComptes();
         } else {
             return null;
         }
     }
-    
+
     /**
      *
      * @param nomClient
      * @return
      */
-    public boolean supprimerClient(String nomClient){
+    public boolean supprimerClient(String nomClient) {
         boolean ok = false;
-        for(int i=0;i<this.clients.size();i++){
-            if(this.clients.get(i).donneNom().equals(nomClient)){
-                if(this.clients.get(i).donneComptes().size()==0){
+        for (int i = 0; i < this.clients.size(); i++) {
+            if (this.clients.get(i).donneNom().equals(nomClient)) {
+                if (this.clients.get(i).donneComptes().size() == 0) {
                     this.clients.remove(i);
                     ok = true;
                 }
@@ -154,17 +160,17 @@ public class Banque {
         }
         return ok;
     }
-    
+
     /**
      *
      * @param numeroCompte
      * @return
      */
-    public boolean supprimerCompte(int numeroCompte){
+    public boolean supprimerCompte(int numeroCompte) {
         boolean ok = false;
-        for(int i=0;i<this.clients.size();i++){
-            for(int j=0;i<this.clients.get(i).donneComptes().size();j++){
-                if(this.clients.get(i).donneComptes().get(j).donneNumero() == numeroCompte){
+        for (int i = 0; i < this.clients.size(); i++) {
+            for (int j = 0; i < this.clients.get(i).donneComptes().size(); j++) {
+                if (this.clients.get(i).donneComptes().get(j).donneNumero() == numeroCompte) {
                     this.clients.get(i).donneComptes().remove(j);
                     ok = true;
                 }
@@ -172,7 +178,7 @@ public class Banque {
         }
         return ok;
     }
-    
+
     /**
      *
      * @param numeroCpteDebiteur
@@ -181,19 +187,19 @@ public class Banque {
      * @param montant
      * @return
      */
-    public boolean transfertInterBancaire(int numeroCpteDebiteur, Banque banqueCrediteur, int numeroCpteCrediteur, double montant){
+    public boolean transfertInterBancaire(int numeroCpteDebiteur, Banque banqueCrediteur, int numeroCpteCrediteur, double montant) {
         CompteBancaire deb = this.rechercheCompte(numeroCpteDebiteur);
         CompteBancaire crd = banqueCrediteur.rechercheCompte(numeroCpteCrediteur);
-        
+
         //effectue les opérations de transfert
-        if(deb!=null&&crd!=null){
+        if (deb != null && crd != null) {
             //vérifie si le montant du débiteur est supérieur au montant à retirer
-            if(deb.consulter()<montant+5.0){
+            if (deb.consulter() < montant + 5.0) {
                 //montant invalide
                 return false;
             } else {
                 //montant valide
-                deb.debiter(montant+5.0);
+                deb.debiter(montant + 5.0);
                 crd.crediter(montant);
             }
             return true;
@@ -201,27 +207,30 @@ public class Banque {
             return false;
         }
     }
-    
+
     /**
-     * Vérifie l'existance d'un client Particulier ou Entreprise dans la liste des clients
+     * Vérifie l'existance d'un client Particulier ou Entreprise dans la liste
+     * des clients
+     *
      * @param c Particulier ou Entreprise
      * @return booléen du test d'existance
      */
-    public boolean clientExiste(Client c){
+    @Deprecated
+    public boolean clientExiste(Client c) {
         boolean existe = false;
-        for(int i=0;i<this.clients.size();i++){
-            if(c instanceof Particulier){
+        for (int i = 0; i < this.clients.size(); i++) {
+            if (c instanceof Particulier) {
                 //si le client recherché est un particulier
                 Particulier p = (Particulier) c;
                 Particulier b = (Particulier) this.clients.get(i);
-                if(p.donneNom().equals(b.donneNom()) && p.donnePrenom().equals(b.donnePrenom())){
+                if (p.donneNom().equals(b.donneNom()) && p.donnePrenom().equals(b.donnePrenom())) {
                     existe = true;
                 }
-            } else if(c instanceof Entreprise){
+            } else if (c instanceof Entreprise) {
                 //si le client recherché est une entreprise
                 Entreprise e = (Entreprise) c;
                 Entreprise b = (Entreprise) this.clients.get(i);
-                if(e.donneNom().equals(b.donneNom()) && e.donneNumeroSIRET() == b.donneNumeroSIRET()){
+                if (e.donneNom().equals(b.donneNom()) && e.donneNumeroSIRET() == b.donneNumeroSIRET()) {
                     existe = true;
                 }
             }
