cdff06b7cfacf528b746159c659899c056913a1b
GDRMc <gdrdu86@gmail.com>
2016-02-04 18:21:16 +0100

    TP3 Banque - Correction issue  #4, ajouts sur structure donnée ArrayList comptes dans classe Banque pour améliorer la gestion des comptes

--- src/gdr/tp/tp3/Banque.java
+++ src/gdr/tp/tp3/Banque.java
@@ -7,35 +7,33 @@ public class Banque {
     private String nomBanque;
 
     private ArrayList<Client> clients;
+    private ArrayList<CompteBancaire> comptes;
 
     /**
-     *
-     * @param nomBanque
+     * Constructeur Banque
+     * @param nomBanque Nom de la Banque
      */
     public Banque(String nomBanque) {
         this.nomBanque = nomBanque;
         this.clients = new ArrayList();
+        this.comptes = new ArrayList();
     }
 
     /**
-     *
-     * @param soldeInitial
-     * @param tauxInteret
-     * @param nomClient
-     * @return
+     * Crée un compte bancaire rémunéré dans la liste des comptes bancaires de la banque
+     * @param soldeInitial solde initial du compte
+     * @param tauxInteret taux d'interêt du compte (prend le dessus sur le taux par défaut)
+     * @param nomClient nom du client (si le client n'existe pas, le compte n'est pas ajouté
+     * @return compte créé
      */
     public CompteBancaireRemunere creerCompteRemunere(double soldeInitial, double tauxInteret, String nomClient) {
         Client c = this.rechercheClient(nomClient);
         boolean insert = false;
         CompteBancaireRemunere compte = null;
         if (c != null) {
-            for (int i = 0; i < this.clients.size(); i++) {
-                if (this.clients.get(i).donneNom().equals(c.donneNom())) {
-                    compte = new CompteBancaireRemunere(soldeInitial, tauxInteret, c);
-                    this.clients.get(i).ajouteCompte(compte);
-                    insert = true;
-                }
-            }
+            compte = new CompteBancaireRemunere(soldeInitial, tauxInteret, c);
+            this.comptes.add(compte);
+            insert = true;
         }
         if (insert) {
             return compte;
@@ -45,14 +43,13 @@ public class Banque {
     }
 
     /**
-     *
-     * @param nomEntreprise
-     * @param numeroSIRET
-     * @param adresse
-     * @return
+     * Ajoute un client de type Entreprise à l'ArrayList des clients
+     * @param nomEntreprise nom de l'entreprise
+     * @param numeroSIRET numéro de SIRET de l'entreprise
+     * @param adresse adresse de l'entreprise
+     * @return entreprise créée
      */
     public Entreprise creerEntreprise(String nomEntreprise, int numeroSIRET, String adresse) {
-        //this.clients.add(new Entreprise(nomEntreprise, adresse, numeroSIRET));
         Entreprise e = new Entreprise(nomEntreprise, adresse, numeroSIRET);
         boolean ok = false;
         if (this.rechercheClient(nomEntreprise) == null) {
@@ -67,11 +64,11 @@ public class Banque {
     }
 
     /**
-     *
-     * @param nomParticulier
-     * @param prenomParticulier
-     * @param adresseParticulier
-     * @return
+     * Ajoute un client de type Particulier à l'ArrayList des clients
+     * @param nomParticulier nom du client
+     * @param prenomParticulier adresse du client
+     * @param adresseParticulier adresse du client
+     * @return particulier créé
      */
     public Particulier creerParticulier(String nomParticulier, String prenomParticulier, String adresseParticulier) {
         Particulier p = new Particulier(nomParticulier, prenomParticulier, adresseParticulier);
@@ -88,9 +85,9 @@ public class Banque {
     }
 
     /**
-     *
-     * @param nom
-     * @return
+     * Recherche d'un client par son nom (Particulier ou Entreprise)
+     * @param nom nom du client (particulier ou entreprise)
+     * @return client (si existant)
      */
     public Client rechercheClient(String nom) {
         boolean trouve = false;
@@ -101,7 +98,7 @@ public class Banque {
                 pos = i;
             }
         }
-        if (trouve) {
+        if (trouve && pos >= 0) {
             return this.clients.get(pos);
         } else {
             return null;
@@ -109,87 +106,86 @@ public class Banque {
     }
 
     /**
-     *
-     * @param num
-     * @return
+     * Recherche d'un compte bancaire dans les comptes clients de la banque
+     * @param num numéro de compte recherché
+     * @return compte (si existant)
      */
     public CompteBancaire rechercheCompte(int num) {
-        int itrouve = -1;
-        int jtrouve = -1;
-        CompteBancaire compte;
-        for (int i = 0; i < this.clients.size(); i++) {
-            for (int j = 0; j < this.clients.get(i).donneComptes().size(); i++) {
-                if (this.clients.get(i).donneComptes().get(j).donneNumero() == num) {
-                    itrouve = i;
-                    jtrouve = j;
-                }
+        int pos = -1;
+        boolean trouve = false;
+        for (int i = 0; i < this.comptes.size(); i++) {
+            if(this.comptes.get(i).donneNumero() == num){
+                pos = i;
             }
         }
-        if (itrouve != -1 && jtrouve != -1) {
-            compte = this.clients.get(itrouve).donneComptes().get(jtrouve);
-        } else {
-            compte = null;
-        }
-        return compte;
-    }
-
-    /**
-     *
-     * @param nomClient
-     * @return
-     */
-    public ArrayList<CompteBancaire> rechercheCompte(String nomClient) {
-        Client c = this.rechercheClient(nomClient);
-        if (c!=null) {
-            return c.donneComptes();
+        if(trouve && pos >= 0){
+            return this.comptes.get(pos);
         } else {
             return null;
         }
     }
 
     /**
-     *
-     * @param nomClient
-     * @return
+     * Recherche d'un ensemble de comptes appartenant à un client
+     * @param nomClient nom du client
+     * @return ArrayList des comptes associés à ce client
      */
-    public boolean supprimerClient(String nomClient) {
-        boolean ok = false;
-        for (int i = 0; i < this.clients.size(); i++) {
-            if (this.clients.get(i).donneNom().equals(nomClient)) {
-                if (this.clients.get(i).donneComptes().size() == 0) {
-                    this.clients.remove(i);
-                    ok = true;
+    public ArrayList<CompteBancaire> rechercheCompte(String nomClient) {
+        Client c = this.rechercheClient(nomClient);
+        ArrayList<CompteBancaire> cpts = new ArrayList();
+        if (c!=null) {
+            for(int i=0;i<this.comptes.size();i++){
+                if(this.comptes.get(i).donneDetenteur().donneNom().equals(nomClient)){
+                    cpts.add(this.comptes.get(i));
                 }
             }
+            return cpts;
+        } else {
+            return null;
         }
-        return ok;
     }
 
     /**
-     *
-     * @param numeroCompte
-     * @return
+     * Supprime un client de la liste s'il n'a pas de comptes associés
+     * @param nomClient nom du client
+     * @return etat de la suppression
+     */
+    public boolean supprimerClient(String nomClient) {
+        boolean peutsupprimer = true;
+        if(this.rechercheCompte(nomClient).size()>0){
+            peutsupprimer = false;
+        }
+        for (int i = 0; i < this.clients.size(); i++) {
+            if (this.clients.get(i).donneNom().equals(nomClient)) {
+                this.clients.remove(i);
+            }
+        }
+        return peutsupprimer;
+    }
+
+    /**
+     * Supprime un compte par le numéro du compte
+     * @param numeroCompte numéro du compte visé
+     * @return etat de la suppression
      */
     public boolean supprimerCompte(int numeroCompte) {
         boolean ok = false;
-        for (int i = 0; i < this.clients.size(); i++) {
-            for (int j = 0; i < this.clients.get(i).donneComptes().size(); j++) {
-                if (this.clients.get(i).donneComptes().get(j).donneNumero() == numeroCompte) {
-                    this.clients.get(i).donneComptes().remove(j);
-                    ok = true;
-                }
+        for (int i = 0; i < this.comptes.size(); i++) {
+            if(this.comptes.get(i).donneNumero() == numeroCompte){
+                this.comptes.remove(i);
+                ok = true;
             }
         }
         return ok;
     }
 
     /**
-     *
-     * @param numeroCpteDebiteur
-     * @param banqueCrediteur
-     * @param numeroCpteCrediteur
-     * @param montant
-     * @return
+     * Transfert interbancaire
+     * @param numeroCpteDebiteur numéro de compte du débiteur
+     * @param banqueCrediteur objet banque du créditeur
+     * @param numeroCpteCrediteur numéro de compte du créditeur dans sa banque
+     * @param montant montant à transférer
+     * @return état de la transaction
      */
     public boolean transfertInterBancaire(int numeroCpteDebiteur, Banque banqueCrediteur, int numeroCpteCrediteur, double montant) {
         CompteBancaire deb = this.rechercheCompte(numeroCpteDebiteur);
